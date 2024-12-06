02a1db4586b2e5fa20a2efc45e7cb9624901d048
ContaDoge <mgerst23@student.aau.dk>
2024-12-06 13:17:05 +0100

    3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,35 +2,104 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RigidbodyMovement : MonoBehaviour {
-    public Rigidbody rb;
-    public float speed = 20f;
-    public float jumpForce = 5f;
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+    public float jumpforce;
+    public float airmultiplier;
 
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground")]
+    public float PlayerHeight;
+    public LayerMask groundMask;
+    public float groundCheckRadius = 0.3f;
+    public Transform groundCheckPoint;
     private bool isGrounded;
 
-    void Update() {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(x, 0, z).normalized;
+    public Transform orientation;
 
-        rb.MovePosition(transform.position + move * speed * Time.deltaTime);
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector3 moveDirection;
 
-        // Jumping
-        if (Input.GetButtonDown("Jump") && isGrounded) {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void Update()
+    {
+        MyInput();
+        SpeedControl();
+
+        isGrounded = Physics.CheckSphere(groundCheckPoint.position, groundCheckRadius, groundMask);
+
+        rb.drag = isGrounded ? groundDrag : 0;
+
+        // Debugging
+        // Debug.Log($"Is Grounded: {isGrounded}");
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(jumpKey) && isGrounded)
+        {
+            Jump();
         }
     }
 
-    void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("Ground")) {
-            isGrounded = true;
+    private void MovePlayer()
+    {
+
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        // Grounded movement
+        if (isGrounded)
+        {
+            rb.velocity = new Vector3(
+                moveDirection.normalized.x * moveSpeed,
+                rb.velocity.y,
+                moveDirection.normalized.z * moveSpeed
+            );
+        }
+        // Airborne movement
+        else
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airmultiplier, ForceMode.Force);
         }
     }
 
-    void OnCollisionExit(Collision collision) {
-        if (collision.gameObject.CompareTag("Ground")) {
-            isGrounded = false;
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
+    }
+
+    private void Jump()
+    {
+        // Reset Y velocity to ensure consistent jump height
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
     }
 }
